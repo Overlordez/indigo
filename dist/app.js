27b47563f3eb4535eaa4b0bd24741d610c3e004f
overlord <dym8750@gmail.com>
2020-09-20 00:51:46 +0300

    сдела блок видео и вопрос-ответ

--- dist/app.js
+++ dist/app.js
@@ -93,8 +93,26 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-$('.carousel slide').carousel({
-  interval: 9000
+$('[data-youtube]').on('click', function () {
+  var id = $(this).data('youtube'),
+      padding = $(window).innerWidth() > 768 ? 120 : 30,
+      ratio = 9 / 16,
+      width = $(window).innerWidth() > 768 ? $(window).innerWidth() - padding - 200 : $(window).innerWidth() - padding,
+      height = width * ratio,
+      html = '<iframe style="width: ' + width + 'px; height: ' + height + 'px;" ' + 'src="' + id + '" frameborder="0" gesture="media" allowfullscreen></iframe>';
+  $('body').append('<div class="outer">' + html + '</div>');
+});
+$(document).mouseup(function (e) {
+  var container = $('.outer iframe');
+
+  if (!container.is(e.target) && container.has(e.target).length === 0) {
+    $('.outer').remove();
+  }
+});
+$(document).on('keyup', function (e) {
+  if (e.keyCode === 27) {
+    $('.outer').remove();
+  }
 });
 
 /***/ }),
--- src/app.js
+++ src/app.js
@@ -1,3 +1,25 @@
-$('.carousel slide').carousel({
-     interval: 9000
-   })
+$('[data-youtube]').on('click', function () {
+     let id = $(this).data('youtube'),
+         padding = $(window).innerWidth() > 768 ? 120 : 30,
+         ratio = 9 / 16,
+         width = $(window).innerWidth() > 768 ? $(window).innerWidth() - padding - 200 : $(window).innerWidth() - padding,
+         height = width * ratio,
+         html = '<iframe style="width: ' + width + 'px; height: ' + height + 'px;" ' +
+             'src="' +
+             id + '" frameborder="0" gesture="media" allowfullscreen></iframe>';
+     $('body').append('<div class="outer">' + html + '</div>');
+ });
+ 
+ $(document).mouseup(function (e) {
+     let container = $('.outer iframe');
+ 
+     if (!container.is(e.target) && container.has(e.target).length === 0) {
+         $('.outer').remove();
+     }
+ });
+ 
+ $(document).on('keyup', function (e) {
+     if (e.keyCode === 27) {
+         $('.outer').remove();
+     }
+ });
